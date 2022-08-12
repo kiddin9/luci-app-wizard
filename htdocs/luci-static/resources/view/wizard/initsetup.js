02d60fc742e3bef876a8676410eac2d1313246a8
Sergey Ponomarev <stokito@gmail.com>
2022-08-12 23:10:39 +0300

    initsetup.js reformat

--- htdocs/luci-static/resources/view/wizard/initsetup.js
+++ htdocs/luci-static/resources/view/wizard/initsetup.js
@@ -18,7 +18,6 @@ return view.extend({
 	},
 
 	render: function(data) {
-
 		var m, s, o;
 		var has_wifi = false;
 
@@ -61,20 +60,20 @@ return view.extend({
 
 		o = s.taboption('netsetup', form.Flag, 'siderouter', _('Siderouter'));
 		o.rmempty = false;
-		
+
 		o = s.taboption('netsetup', form.Value, 'lan_gateway', _('IPv4 gateway'));
 		o.depends('siderouter', '1');
 		o.datatype = 'ip4addr';
 		o.placeholder = '请输入主路由IP';
 		o.rmempty = false;
-		
+
 		o = s.taboption('netsetup', form.Flag, 'dhcp', _('DHCP Server'), _('开启此DHCP则需要关闭主路由的DHCP, 关闭此DHCP则需要手动将所有上网设备的网关和DNS改为此旁路由的IP'));
 		o.depends('siderouter', '1');
 		o.default = o.enabled;
-		
+
 		o = s.taboption('netsetup', form.Flag, 'ipv6', _('Enable IPv6'), _('Enable/Disable IPv6'));
 		o.default = o.enabled;
-		
+
 		s.tab('firmware', _('Firmware Settings'));
 
 		// o = s.taboption('firmware', form.Flag, 'autoupgrade_pkg', _('Packages Auto Upgrade'),_('谨慎开启'));
@@ -82,13 +81,13 @@ return view.extend({
 
 		o = s.taboption('firmware', form.Flag, 'autoupgrade_fm', _('Firmware Upgrade Notice'));
 		o.rmempty = false;
-		
-		o = s.taboption('firmware', form.Flag, 'coremark', _('CoreMark'),_('第一次开机后是否运行CPU跑分测试'));
+
+		o = s.taboption('firmware', form.Flag, 'coremark', _('CoreMark'), _('第一次开机后是否运行CPU跑分测试'));
 		o.rmempty = false;
-		
-		o = s.taboption('firmware', form.Flag, 'cookie_p', _('Persistent cookies'),_('保持后台登录状态,避免每次关闭浏览器后都需要重新登录'));
+
+		o = s.taboption('firmware', form.Flag, 'cookie_p', _('Persistent cookies'), _('保持后台登录状态,避免每次关闭浏览器后都需要重新登录'));
 		o.default = o.enabled;
-		
+
 		if (has_wifi) {
 			s.tab('wifisetup', _('Wireless Settings'), _('Set the router\'s wireless name and password. For more advanced settings, please go to the Network-Wireless page.'));
 			o = s.taboption('wifisetup', form.Value, 'wifi_ssid', _('<abbr title=\"Extended Service Set Identifier\">ESSID</abbr>'));
@@ -98,9 +97,9 @@ return view.extend({
 			o.datatype = 'wpakey';
 			o.password = true;
 		}
-		
-		setTimeout("document.getElementsByClassName('cbi-button-apply')[0].children[3].children[0].value='1'",1000)
-		
+
+		setTimeout("document.getElementsByClassName('cbi-button-apply')[0].children[3].children[0].value='1'", 1000)
+
 		return m.render();
 	}
 });
